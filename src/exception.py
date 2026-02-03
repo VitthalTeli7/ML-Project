d0b365355cb6a66813fc0bff64a1d1435b9c2f0b
Vitthal Teli <vitthalteli7001@gmail.com>
2026-02-03 13:14:35 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,24 +1,22 @@
 import sys
-import logging
-
-# Setup basic logging (you can replace this with logger_setup import if you have it)
-logging.basicConfig(
-    filename="logs/errors.log",  # this file will be created
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+from src.logger import logging  
 
 def error_message_detail(error, error_detail: sys):
+    """Generate detailed error message including file name and line number."""
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = f"Error occurred in Python script: {file_name} at line number: {exc_tb.tb_lineno} with message: {str(error)}"
+    error_message = f"Error occurred in python script name: [{0}] at line: [{1}] | Error message: [{2}]"
+    file_name,exc_tb.tb_lineno,str(error)
+
     return error_message
 
 class CustomException(Exception):
+    """Custom Exception class that includes detailed error information."""
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
-        return self.error_message
-
+        return self.error_message   
+    
+    
